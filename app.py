9188849ff90a29a1437439bd9cc60f6d1caaf718
sfbenitez <sergioferretebenitez@gmail.com>
2017-12-01 11:02:42 +0100

    first test

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import psycopg2
 
 conn = psycopg2.connect("dbname=db_backup user=admin password=admin host=172.22.2.205")
 
-def database_query(sql_query):
+def database_insert(sql_query):
 	cur = None
 	print sql_query
 	try:
@@ -22,9 +22,22 @@ def database_query(sql_query):
 			cur.close()
     return cur.statusmessage
 
+def database_select(sql_query):
+	cur = None
+	print sql_query
+	try:
+		cur = conn.cursor()
+		cur.execute(sql_query)
+		resultado = cur.fetchone()
+	finally:
+		if cur is not None:
+			cur.close()
+	return resultado
+
 @route('/')
 def index():
-    sql_select="SELECT * FROM "
-    database_query(sql_select)
+    sql_select="SELECT * FROM USERS"
+    campos=database_select(sql_select)
     # ver como enviar campos del select al template
-    return template("index.tpl")
+
+    return template("index.tpl",  user_user=campos[0], user_name=campos[1])
---
+++ index.tpl
@@ -0,0 +1,2 @@
+<h1>Consulta sobre: {{user_user}}</h1>
+<h2>{{user_name}}</h2>
