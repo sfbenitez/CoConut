4dc25be7af43e64771ec3f07e4b27353d159ecb4
sfbenitez <sergioferretebenitez@gmail.com>
2017-12-01 12:58:14 +0100

    static files tab

--- app/app.py
+++ app/app.py
@@ -43,7 +43,7 @@ def index():
 
 # Static files
 @route('/static/<filename>')
-	def server_static(filename):
-		return static_file(filename, root='/static/')
+def server_static(filename):
+	return static_file(filename, root='/static/')
 
 run(host = '0.0.0.0', port = 8080)
