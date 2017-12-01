7a2a417a2bdbd63d785f38d9e4409025e48d1fac
sfbenitez <sergioferretebenitez@gmail.com>
2017-12-01 12:20:01 +0100

    comenting insert function

--- app/app.py
+++ app/app.py
@@ -5,22 +5,22 @@ import psycopg2
 
 conn = psycopg2.connect("dbname=db_backup user=admin password=admin host=172.22.2.205")
 
-def database_insert(sql_query):
-	cur = None
-	print sql_query
-	try:
-		cur = conn.cursor()
-		cur.execute(sql_query)
-		resultado = cur.statusmessage
-		conn.commit()
-	except Exception , e:
-		print 'ERROR:', e[0]
-		if cur is not None:
-			conn.rollback()
-	finally:
-		if cur is not None:
-			cur.close()
-    return cur.statusmessage
+#def database_insert(sql_query):
+#	cur = None
+#	print sql_query
+#	try:
+#		cur = conn.cursor()
+#		cur.execute(sql_query)
+#		resultado = cur.statusmessage
+#		conn.commit()
+#	except Exception , e:
+#		print 'ERROR:', e[0]
+#		if cur is not None:
+#			conn.rollback()
+#	finally:
+#		if cur is not None:
+#			cur.close()#
+#    return cur.statusmessage
 
 def database_select(sql_query):
 	cur = None
