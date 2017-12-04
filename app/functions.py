9a272c7890309cca4eadfe829ef36e4b5cc72ea4
Carlos Jesús Sánchez Ortega <carlosjsanchezortega@gmail.com>
2017-12-04 10:10:15 +0100

    comment insert sql

--- app/functions.py
+++ app/functions.py
@@ -36,19 +36,19 @@ def database_select(sql_query):
 			cur.close()
 	return resultado
 
-def database_insert(sql_query):
-	cur = None
-	print sql_query
-	try:
-		cur = conn.cursor()
-		cur.execute(sql_query)
-		resultado = cur.statusmessage
-		conn.commit()
-	except Exception , e:
-		print 'ERROR:', e[0]
-		if cur is not None:
-			conn.rollback()
-	finally:
-		if cur is not None:
-			cur.close()#
-    return cur.statusmessage
+#def database_insert(sql_query):
+#	cur = None
+#	print sql_query
+#	try:
+#		cur = conn.cursor()
+#		cur.execute(sql_query)
+#		resultado = cur.statusmessage
+#		conn.commit()
+#	except Exception , e:
+#		print 'ERROR:', e[0]
+#		if cur is not None:
+#			conn.rollback()
+#	finally:
+#		if cur is not None:
+#			cur.close()#
+#    return cur.statusmessage
